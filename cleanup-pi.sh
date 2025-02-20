5d9908edc1cb56e6ae330c3c9623e00388982fde
Joeri van Dooren <joeri@weepee.org>
2025-02-20 13:58:03 +0100

    Update cleanup-pi.sh

--- cleanup-pi.sh
+++ cleanup-pi.sh
@@ -9,6 +9,7 @@ cd /var/log
 
 journalctl --rotate
 journalctl --vacuum-time=1s
+rm -fr /root/.cache
 find . | grep "\.[1-9]$" | grep -v journal | xargs rm
 find . | grep "\.log$" | xargs truncate -s0
 truncate -s0 svxlink
